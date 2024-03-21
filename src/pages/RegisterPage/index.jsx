f5d4c88ec2ac68e0bb3369097423f7db3b394f9b
AthulKkumar <athulkkumar5@gmail.com>
2024-03-21 13:30:47 +0530

    fix(register): add fix in forgotpassword

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -66,7 +66,6 @@ const Signin = () => {
   //   return <h1>Please Login</h1>;
   // }
 
-
   const [showPassword, setShowPassword] = useState(false);
 
   return (
@@ -123,7 +122,6 @@ const Signin = () => {
                   }
                 />
               </div>
-
               <div className="mb-3">
                 <input
                   type="password"
@@ -146,8 +144,7 @@ const Signin = () => {
                 Sign In
               </button>
               {/* </Link> */}
-
-//               <div className="mb-3  input-group   ">
+              {/* //               <div className="mb-3  input-group   ">
 //                 <div
 //                   style={{ padding: " 0px" }}
 //                   className="form-control d-flex  justify-content-center  align-items-center "
@@ -178,8 +175,7 @@ const Signin = () => {
 //                 >
 //                   Sign In
 //                 </button>
-//               </Link>
-
+//               </Link> */}
             </section>
 
             <div className="d-flex justify-content-between gap-5 ">
@@ -215,84 +211,84 @@ const Signin = () => {
   );
 };
 
-
-function ForgetPassword() {
-  const [nextPage, setNextPage] = useState(true);
-  const [email, setEmail] = useState("");
-  const navigate = useNavigate();
-
-  function sendVerifcationLink() {
-    setNextPage(false);
-    authApi.handleVerificationEmail({
-      payload: { email },
-      success: (res) => {
-        console.log("Verification Email Success", res);
-      },
-      error: (err) => {
-        console.log("Verification Email Error", err);
-      },
-    });
-  }
-
-  return (
-    <div
-      className={`container bg-white position-absolute  top-50 start-50 translate-middle d-flex p-2 rounded   border text-black `}
-      style={{ width: " -webkit-fill-available" }}
-    >
-      <div className="d-flex flex-column justify-content-between align-items-start p-4 gap-3 w-100">
-        <button
-          className="btn btn-secondary"
-          onClick={() => navigate("/register")}
-        >
-          Go Back
-        </button>
-        <section className="d-flex flex-column justify-content-center align-items-center gap-3 mb-5 w-100 ">
-          <img src={logo2} alt="" />
-          <h1>Forget Password ?</h1>
-          {nextPage ? (
-            <>
-              <p className={styles.forgotPasswordPara}>
-                Don't worry! Resetting your password is simple. Just type in the
-                email you registered to Record.
-              </p>
-              <section className="d-flex flex-column justify-content-center align-items-strech w-50">
-                <div className="mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="formGroupExampleInput"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={sendVerifcationLink}
-                >
-                  Send
-                </button>
-              </section>
-              <p>Don’t have an account? Contact Administrator</p>
-            </>
-          ) : (
-            <>
-              <p>Great, password reset link has been sent to your email.</p>
-              <section className="d-flex flex-column justify-content-center align-items-strech w-50">
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={() => navigate("/register")}
-                >
-                  Sign In Now
-                </button>
-              </section>
-            </>
-          )}
-        </section>
-
 // function ForgetPassword() {
+//   const [nextPage, setNextPage] = useState(true);
+//   const [email, setEmail] = useState("");
+//   const navigate = useNavigate();
+
+//   function sendVerifcationLink() {
+//     setNextPage(false);
+//     authApi.handleVerificationEmail({
+//       payload: { email },
+//       success: (res) => {
+//         console.log("Verification Email Success", res);
+//       },
+//       error: (err) => {
+//         console.log("Verification Email Error", err);
+//       },
+//     });
+//   }
+
+//   return (
+//     <div
+//       className={`container bg-white position-absolute  top-50 start-50 translate-middle d-flex p-2 rounded   border text-black `}
+//       style={{ width: " -webkit-fill-available" }}
+//     >
+//       <div className="d-flex flex-column justify-content-between align-items-start p-4 gap-3 w-100">
+//         <button
+//           className="btn btn-secondary"
+//           onClick={() => navigate("/register")}
+//         >
+//           Go Back
+//         </button>
+//         <section className="d-flex flex-column justify-content-center align-items-center gap-3 mb-5 w-100 ">
+//           <img src={logo2} alt="" />
+//           <h1>Forget Password ?</h1>
+//           {nextPage ? (
+//             <>
+//               <p className={styles.forgotPasswordPara}>
+//                 Don't worry! Resetting your password is simple. Just type in the
+//                 email you registered to Record.
+//               </p>
+//               <section className="d-flex flex-column justify-content-center align-items-strech w-50">
+//                 <div className="mb-3">
+//                   <input
+//                     type="email"
+//                     className="form-control"
+//                     id="formGroupExampleInput"
+//                     placeholder="Email"
+//                     value={email}
+//                     onChange={(e) => setEmail(e.target.value)}
+//                   />
+//                 </div>
+//                 <button
+//                   type="button"
+//                   className="btn btn-primary"
+//                   onClick={sendVerifcationLink}
+//                 >
+//                   Send
+//                 </button>
+//               </section>
+//               <p>Don’t have an account? Contact Administrator</p>
+//             </>
+//           ) : (
+//             <>
+//               <p>Great, password reset link has been sent to your email.</p>
+//               <section className="d-flex flex-column justify-content-center align-items-strech w-50">
+//                 <button
+//                   type="button"
+//                   className="btn btn-primary"
+//                   onClick={() => navigate("/register")}
+//                 >
+//                   Sign In Now
+//                 </button>
+//               </section>
+//             </>
+//           )}
+//         </section>
+
+{
+  /* // function ForgetPassword() {
 //   const [email, setEmail] = useState(true);
 //   const navigate = useNavigate();
 
@@ -360,6 +356,7 @@ function ForgetPassword() {
 //       </div>
 //     </div>
 //   );
-// }
+// } */
+}
 
 export default Signin;
